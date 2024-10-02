dc913b3bf124b3787e1e1f994c51c15af2683771
Alexander R Torrijos <artorrijos@gmail.com>
2024-10-02 10:37:53 +0800

    Initial Commit

---
+++ src/FileProcessor/Program.cs
@@ -0,0 +1,51 @@
+﻿using static System.Console;
+// See https://aka.ms/new-console-template for more information
+
+WriteLine("File Processor");
+WriteLine("--------------");
+
+while (true)
+{
+    WriteLine();
+    WriteLine("(V)erify Only ");
+    WriteLine("(I)mport Into Database ");
+    WriteLine("(P)rint ");
+    WriteLine("(E)xit ");
+
+    WriteLine("Enter choice: ");
+
+    var input = ReadLine();
+
+    switch (input.ToUpper())
+    {
+        case "V":
+            Verify();
+            break;
+        case "I":
+            Import();
+            break;
+        case "P":
+            Print();
+            break;
+        case "E":
+            return;
+        default:
+            WriteLine("Invalid choice. Please try again.");
+            break;
+    }
+}
+
+void Print()
+{
+    WriteLine("Printing...");
+}
+
+void Import()
+{
+    WriteLine("Importing...");
+}
+
+void Verify()
+{
+    WriteLine("Verifying...");
+}
